0916e9759315f8fd29f4963147ec4e6d4ad29d9e
Andrey22250 <andrchepras@mail.ru>
2023-11-30 01:02:56 +0700

    Ah shit, here we go again

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //пример работы с PC
+        PC[] pcList = new PC[2];
+
+        pcList[0] = new PC();
+        pcList[0].input_pc();
+        System.out.println();
+        pcList[1] = new PC();
+        pcList[1].input_pc();
+        System.out.println();
+
+        pcList[0].out_PC();
+        pcList[1].out_PC();
+        pcList[0].Undervolt_GPU();
+        pcList[0].out_PC();
+
+        //пример работы с Build
+        Build BuildEx = new Build("Андрей");
+        BuildEx.out_build();
+        BuildEx.ChangeStatus(Status.FINISHED);
+        BuildEx.out_build();
+    }
+}
